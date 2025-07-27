e3a613b78dfcd213cd817333e2c34a11a829a410
ReyanNaseem <reyannaseem516@gmail.com>
2025-07-27 12:57:06 +0500

    all setup configure

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,20 @@
-const asyncHandler = (requestHandler)=>{async(req,res,next)=>{
-    try {
-        await requestHandler(req,res,next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}}
-
-export { asyncHandler };
-
-// const asyncHandler = (requestHandler)=>{
-//     (req,res,next)=>{
-//         Promise.resolve( req, res, next )
-//         .catch((err)=>{
-//             next(err)
+// const asyncHandler = (requestHandler)=>{async(req,res,next)=>{
+//     try {
+//         await requestHandler(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
 //         })
 //     }
-// };
+// }}
+
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+      .catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
